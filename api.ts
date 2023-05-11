49b4a6cbca1b25166a3950c08eafe14f18bec865
Simon Wörpel <simon.woerpel@pm.me>
2023-05-11 14:12:05 +0200

    Override 404 api error

--- api.ts
+++ api.ts
@@ -1,7 +1,11 @@
+import { notFound } from "next/navigation";
+
 import Api from "~/lib/ftm/api";
 
 import { API_ENDPOINT } from "~/config";
 
 const api = new Api(API_ENDPOINT);
 
-export default api
+api.onNotFound = () => notFound();
+
+export default api;

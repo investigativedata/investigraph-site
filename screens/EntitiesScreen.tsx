b871ec80c153750417e1e3dbe79771165088f0f5
Simon Wörpel <simon.woerpel@pm.me>
2023-05-11 17:13:41 +0200

    🚧 Use EntityCards in list screen

--- screens/EntitiesScreen.tsx
+++ screens/EntitiesScreen.tsx
@@ -4,7 +4,7 @@ import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
 import Stack from "@mui/joy/Stack";
 
-import { DatasetHeader, EntityLink } from "~/lib/ftm/components";
+import { DatasetHeader, EntityCard } from "~/lib/ftm/components";
 import type { INKDataset, TEntity } from "~/lib/ftm/types";
 
 import { Headline } from "~/components/common/typo";
@@ -23,7 +23,9 @@ export default function EntitiesScreen(props: Props) {
       </Headline>
       <List>
         {props.entities.map((e) => (
-          <EntityLink key={e.id} entity={e} icon />
+          <ListItem key={e.id} style={{ display: "block"}}>
+            <EntityCard entity={e} />
+          </ListItem>
         ))}
       </List>
     </Stack>

6f7e1f238c07d9de8f9922f22fd0e7b51817fca6
Simon Wörpel <simon.woerpel@pm.me>
2023-07-18 07:44:17 +0200

    📝 (index) append slash to avoid github.io redirect

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           </p>
         </div>
         <div className={styles.overviewItem}>
-          <a className={styles.actionButton} href="https://docs.investigraph.dev/tutorial">
+          <a className={styles.actionButton} href="https://docs.investigraph.dev/tutorial/">
             get started
           </a>
         </div>
